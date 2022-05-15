f977b4908bd91e7140d350d5e9c327081e5048da
Vladimir Kozhukalov <kozhukalov@gmail.com>
2022-05-15 23:25:04 +0300

    Change tabs to whitespaces

--- master/CANopen/master_cltrans.c
+++ master/CANopen/master_cltrans.c
@@ -8,148 +8,148 @@ static unsigned32 sdo_timeout_mcs;
 
 static void resetcanwork(void)
 {
-	cltbasic.ct.ss.state = CAN_TRANSTATE_OK;
-	cltbasic.ct.ss.abortcode = 0;
-	cltbasic.timeout = 1 + CAN_TIMEOUT_RETRIEVE / CAN_TIMERUSEC;		// 2.2.1
-	cltbasic.capture = 0;
-	cltbasic.busy = -1;
+    cltbasic.ct.ss.state = CAN_TRANSTATE_OK;
+    cltbasic.ct.ss.abortcode = 0;
+    cltbasic.timeout = 1 + CAN_TIMEOUT_RETRIEVE / CAN_TIMERUSEC;        // 2.2.1
+    cltbasic.capture = 0;
+    cltbasic.busy = -1;
 }
 
 void can_client_control(void)
 {
-	sem_resetcan++;
-	if (sem_resetcan != 0) {
-		sem_resetcan--;
-		flag_resetcan++;
-		return;
-	}
-	if (cltbasic.busy >= 0) {
-		cltbasic.timeout--;
-		if (cltbasic.timeout == 0) {
-			if (cltbasic.ct.ss.state == CAN_TRANSTATE_SDO_WORK) {
-				abort_can_sdo(&cltbasic.ct.sd.si, CAN_ABORT_SDO_TIMEOUT);
-				cltbasic.ct.ss.state = CAN_TRANSTATE_SDO_NET_TIMEOUT;
-				cltbasic.timeout = 1 + CAN_TIMEOUT_READ / CAN_TIMERUSEC;		// 2.2.1
-			} else {
-				resetcanwork();
-			}
-		}
-	}
-	sem_resetcan--;
+    sem_resetcan++;
+    if (sem_resetcan != 0) {
+        sem_resetcan--;
+        flag_resetcan++;
+        return;
+    }
+    if (cltbasic.busy >= 0) {
+        cltbasic.timeout--;
+        if (cltbasic.timeout == 0) {
+            if (cltbasic.ct.ss.state == CAN_TRANSTATE_SDO_WORK) {
+                abort_can_sdo(&cltbasic.ct.sd.si, CAN_ABORT_SDO_TIMEOUT);
+                cltbasic.ct.ss.state = CAN_TRANSTATE_SDO_NET_TIMEOUT;
+                cltbasic.timeout = 1 + CAN_TIMEOUT_READ / CAN_TIMERUSEC;        // 2.2.1
+            } else {
+                resetcanwork();
+            }
+        }
+    }
+    sem_resetcan--;
 }
 
 static void contcan_lock(void)
 {
-	sem_resetcan++;
+    sem_resetcan++;
 }
 
 static void contcan_unlock(void)
 {
-	sem_resetcan--;
-	while (flag_resetcan > 0) {
-		if (sem_resetcan != -1) break;
-		can_client_control();
-		flag_resetcan--;
-	}
+    sem_resetcan--;
+    while (flag_resetcan > 0) {
+        if (sem_resetcan != -1) break;
+        can_client_control();
+        flag_resetcan--;
+    }
 }
 
 void can_client_sdo(canframe *cf)
 {
-	struct cansdo sd;
+    struct cansdo sd;
 
-	contcan_lock();
-	if (cltbasic.ct.ss.state == CAN_TRANSTATE_SDO_WORK) {
-		parse_sdo(&sd, cf->data);
-		if (sd.cs == CAN_CS_SDO_ABORT) {
-			cltbasic.ct.ss.abortcode = canframe_to_u32(sd.bd);
-			cltbasic.ct.ss.state = CAN_TRANSTATE_SDO_SRVABORT;
-		} else if (sd.cs == cltbasic.ct.adjcs) {
-			cltbasic.ct.sd = sd;
-			cltbasic.ct.ss.state = CAN_TRANSTATE_OK;
-		} else {
-			abort_can_sdo(&cltbasic.ct.sd.si, CAN_ABORT_SDO_CS);
-			cltbasic.ct.ss.state = CAN_TRANSTATE_SDO_SCSERR;
-		}
-		cltbasic.timeout = 1 + CAN_TIMEOUT_READ / CAN_TIMERUSEC;		// 2.2.1
-	}
-	contcan_unlock();
+    contcan_lock();
+    if (cltbasic.ct.ss.state == CAN_TRANSTATE_SDO_WORK) {
+        parse_sdo(&sd, cf->data);
+        if (sd.cs == CAN_CS_SDO_ABORT) {
+            cltbasic.ct.ss.abortcode = canframe_to_u32(sd.bd);
+            cltbasic.ct.ss.state = CAN_TRANSTATE_SDO_SRVABORT;
+        } else if (sd.cs == cltbasic.ct.adjcs) {
+            cltbasic.ct.sd = sd;
+            cltbasic.ct.ss.state = CAN_TRANSTATE_OK;
+        } else {
+            abort_can_sdo(&cltbasic.ct.sd.si, CAN_ABORT_SDO_CS);
+            cltbasic.ct.ss.state = CAN_TRANSTATE_SDO_SCSERR;
+        }
+        cltbasic.timeout = 1 + CAN_TIMEOUT_READ / CAN_TIMERUSEC;        // 2.2.1
+    }
+    contcan_unlock();
 }
 
 static void readcbas(struct sdoclttrans *ct)
 {
-	if (cltbasic.busy < 0) {
-		ct->ss.state = CAN_TRANSTATE_SDO_READ_TIMEOUT;
-		return;
-	}
-	contcan_lock();
-	if (cltbasic.ct.ss.state != CAN_TRANSTATE_SDO_WORK) {
-		*ct = cltbasic.ct;
-		resetcanwork();
-	}
-	contcan_unlock();
+    if (cltbasic.busy < 0) {
+        ct->ss.state = CAN_TRANSTATE_SDO_READ_TIMEOUT;
+        return;
+    }
+    contcan_lock();
+    if (cltbasic.ct.ss.state != CAN_TRANSTATE_SDO_WORK) {
+        *ct = cltbasic.ct;
+        resetcanwork();
+    }
+    contcan_unlock();
 }
 
 static void request_client_trans(struct sdoclttrans *ct)
 {
-	ct->ss.state = CAN_TRANSTATE_SDO_WORK;
-	ct->ss.abortcode = 0;
-	contcan_lock();
-	CAN_CRITICAL_BEGIN
-	cltbasic.busy++;
-	if (cltbasic.busy == 0) {
-		CAN_CRITICAL_END
-		if (cltbasic.capture == 0) {
-			cltbasic.capture = 1;
-			cltbasic.ct = *ct;
-			cltbasic.ct.ss.state = CAN_TRANSTATE_SDO_WORK;
-			if (send_can_sdo(&ct->sd) != CAN_RETOK) {
-				ct->ss.state = CAN_TRANSTATE_SDO_WRITERR;
-				resetcanwork();
-			}
-		}
-	} else {
-		cltbasic.busy--;
-		CAN_CRITICAL_END
-		ct->ss.state = CAN_TRANSTATE_SDO_NOWORKB;
-	}
-	contcan_unlock();
+    ct->ss.state = CAN_TRANSTATE_SDO_WORK;
+    ct->ss.abortcode = 0;
+    contcan_lock();
+    CAN_CRITICAL_BEGIN
+    cltbasic.busy++;
+    if (cltbasic.busy == 0) {
+        CAN_CRITICAL_END
+        if (cltbasic.capture == 0) {
+            cltbasic.capture = 1;
+            cltbasic.ct = *ct;
+            cltbasic.ct.ss.state = CAN_TRANSTATE_SDO_WORK;
+            if (send_can_sdo(&ct->sd) != CAN_RETOK) {
+                ct->ss.state = CAN_TRANSTATE_SDO_WRITERR;
+                resetcanwork();
+            }
+        }
+    } else {
+        cltbasic.busy--;
+        CAN_CRITICAL_END
+        ct->ss.state = CAN_TRANSTATE_SDO_NOWORKB;
+    }
+    contcan_unlock();
 }
 
 void can_client_basic(struct sdoclttrans *ct)
 {
-	unsigned32 tout;
+    unsigned32 tout;
 
-	request_client_trans(ct);
-	if (ct->ss.state != CAN_TRANSTATE_SDO_WORK) return;
-	cltbasic.timeout = 1 + sdo_timeout_mcs / CAN_TIMERUSEC;
-	tout = 2 * (1 + sdo_timeout_mcs / CAN_SLEEP_READ);			// 2.2.1
-	while (tout > 0) {
-		can_sleep(CAN_SLEEP_READ);
-		readcbas(ct);
-		if (ct->ss.state != CAN_TRANSTATE_SDO_WORK) return;
-		tout--;
-	}
-	ct->ss.state = CAN_TRANSTATE_SDO_TRANS_TIMEOUT;
+    request_client_trans(ct);
+    if (ct->ss.state != CAN_TRANSTATE_SDO_WORK) return;
+    cltbasic.timeout = 1 + sdo_timeout_mcs / CAN_TIMERUSEC;
+    tout = 2 * (1 + sdo_timeout_mcs / CAN_SLEEP_READ);            // 2.2.1
+    while (tout > 0) {
+        can_sleep(CAN_SLEEP_READ);
+        readcbas(ct);
+        if (ct->ss.state != CAN_TRANSTATE_SDO_WORK) return;
+        tout--;
+    }
+    ct->ss.state = CAN_TRANSTATE_SDO_TRANS_TIMEOUT;
 }
 
 void set_sdo_timeout(unsigned32 microseconds)
 {
-	if (microseconds < CAN_TIMERUSEC) sdo_timeout_mcs = CAN_TIMERUSEC;
-	else sdo_timeout_mcs = microseconds;
+    if (microseconds < CAN_TIMERUSEC) sdo_timeout_mcs = CAN_TIMERUSEC;
+    else sdo_timeout_mcs = microseconds;
 }
 
 unsigned32 get_sdo_timeout(void)
 {
-	return sdo_timeout_mcs;
+    return sdo_timeout_mcs;
 }
 
 void can_init_client(void)
 {
- 	sem_resetcan = 0;
- 	flag_resetcan = 0;
-	resetcanwork();
-	sdo_timeout_mcs = CAN_TIMEOUT_RETRIEVE;
- 	sem_resetcan = -1;
+     sem_resetcan = 0;
+     flag_resetcan = 0;
+    resetcanwork();
+    sdo_timeout_mcs = CAN_TIMEOUT_RETRIEVE;
+     sem_resetcan = -1;
 }
 
 #endif
--- master/application/master_can_nodes.c
+++ master/application/master_can_nodes.c
@@ -4,82 +4,82 @@
 
 static int16 check_node_valid(cannode node)
 {
-	unsigned32 devdata;
+    unsigned32 devdata;
 
-	if (can_node[node].maskdev & MASK_DEV_DEVICETYPE) {
-		if (read_device_object(node, CAN_INDEX_DEVICE_TYPE, 0, (canbyte*)&devdata, 4) != CAN_TRANSTATE_OK) {
-			return NOT_VALID;
-		}
-		if (devdata != can_node[node].DeviceType) {
-			node_event(node, EVENT_CLASS_NODE_CONFIG, EVENT_TYPE_ERROR, EVENT_CODE_DEVICE_TYPE, devdata);
-			return NOT_VALID;
-		}
-	}
-	if (can_node[node].maskdev & MASK_DEV_VENDORID) {
-		if (read_device_object(node, CAN_INDEX_IDENTITY, 1, (canbyte*)&devdata, 4) != CAN_TRANSTATE_OK) {
-			return NOT_VALID;
-		}
-		if (devdata != can_node[node].VendorID) {
-			node_event(node, EVENT_CLASS_NODE_CONFIG, EVENT_TYPE_ERROR, EVENT_CODE_VENDOR_ID, devdata);
-			return NOT_VALID;
-		}
-	}
-	if (can_node[node].maskdev & MASK_DEV_PRODUCTCODE) {
-		if (read_device_object(node, CAN_INDEX_IDENTITY, 2, (canbyte*)&devdata, 4) != CAN_TRANSTATE_OK) {
-			return NOT_VALID;
-		}
-		if (devdata != can_node[node].ProductCode) {
-			node_event(node, EVENT_CLASS_NODE_CONFIG, EVENT_TYPE_ERROR, EVENT_CODE_PRODUCT_CODE, devdata);
-			return NOT_VALID;
-		}
-	}
-	if (can_node[node].maskdev & MASK_DEV_REVISION) {
-		if (read_device_object(node, CAN_INDEX_IDENTITY, 3, (canbyte*)&devdata, 4) != CAN_TRANSTATE_OK) {
-			return NOT_VALID;
-		}
-		if (devdata != can_node[node].Revision) {
-			node_event(node, EVENT_CLASS_NODE_CONFIG, EVENT_TYPE_ERROR, EVENT_CODE_REVISION, devdata);
-			return NOT_VALID;
-		}
-	}
-	if (can_node[node].maskdev & MASK_DEV_SERIAL) {
-		if (read_device_object(node, CAN_INDEX_IDENTITY, 4, (canbyte*)&devdata, 4) != CAN_TRANSTATE_OK) {
-			return NOT_VALID;
-		}
-		if (devdata != can_node[node].Serial) {
-			node_event(node, EVENT_CLASS_NODE_CONFIG, EVENT_TYPE_ERROR, EVENT_CODE_SERIAL, devdata);
-			return NOT_VALID;
-		}
-	}
-	return VALID;
+    if (can_node[node].maskdev & MASK_DEV_DEVICETYPE) {
+        if (read_device_object(node, CAN_INDEX_DEVICE_TYPE, 0, (canbyte*)&devdata, 4) != CAN_TRANSTATE_OK) {
+            return NOT_VALID;
+        }
+        if (devdata != can_node[node].DeviceType) {
+            node_event(node, EVENT_CLASS_NODE_CONFIG, EVENT_TYPE_ERROR, EVENT_CODE_DEVICE_TYPE, devdata);
+            return NOT_VALID;
+        }
+    }
+    if (can_node[node].maskdev & MASK_DEV_VENDORID) {
+        if (read_device_object(node, CAN_INDEX_IDENTITY, 1, (canbyte*)&devdata, 4) != CAN_TRANSTATE_OK) {
+            return NOT_VALID;
+        }
+        if (devdata != can_node[node].VendorID) {
+            node_event(node, EVENT_CLASS_NODE_CONFIG, EVENT_TYPE_ERROR, EVENT_CODE_VENDOR_ID, devdata);
+            return NOT_VALID;
+        }
+    }
+    if (can_node[node].maskdev & MASK_DEV_PRODUCTCODE) {
+        if (read_device_object(node, CAN_INDEX_IDENTITY, 2, (canbyte*)&devdata, 4) != CAN_TRANSTATE_OK) {
+            return NOT_VALID;
+        }
+        if (devdata != can_node[node].ProductCode) {
+            node_event(node, EVENT_CLASS_NODE_CONFIG, EVENT_TYPE_ERROR, EVENT_CODE_PRODUCT_CODE, devdata);
+            return NOT_VALID;
+        }
+    }
+    if (can_node[node].maskdev & MASK_DEV_REVISION) {
+        if (read_device_object(node, CAN_INDEX_IDENTITY, 3, (canbyte*)&devdata, 4) != CAN_TRANSTATE_OK) {
+            return NOT_VALID;
+        }
+        if (devdata != can_node[node].Revision) {
+            node_event(node, EVENT_CLASS_NODE_CONFIG, EVENT_TYPE_ERROR, EVENT_CODE_REVISION, devdata);
+            return NOT_VALID;
+        }
+    }
+    if (can_node[node].maskdev & MASK_DEV_SERIAL) {
+        if (read_device_object(node, CAN_INDEX_IDENTITY, 4, (canbyte*)&devdata, 4) != CAN_TRANSTATE_OK) {
+            return NOT_VALID;
+        }
+        if (devdata != can_node[node].Serial) {
+            node_event(node, EVENT_CLASS_NODE_CONFIG, EVENT_TYPE_ERROR, EVENT_CODE_SERIAL, devdata);
+            return NOT_VALID;
+        }
+    }
+    return VALID;
 }
 
 static void can_node_config(cannode node)
 {
-	unsigned16 hbt;
+    unsigned16 hbt;
 
-	can_node[node].nmt_state = CAN_NODE_STATE_UNCERTAIN;
-	if (check_node_valid(node) != VALID) return;
-	hbt = CAN_HBT_PRODUCER_MS;
-	if (write_device_object(node, CAN_INDEX_PROD_HBT, 0, (canbyte*)&hbt, 2) != CAN_TRANSTATE_OK) {
-		node_event(node, EVENT_CLASS_NODE_CONFIG, EVENT_TYPE_ERROR, EVENT_CODE_PROD_HBT, EVENT_INFO_DUMMY);
-		return;
-	}
-	can_node[node].nmt_state = CAN_NODE_STATE_PRE_OPERATIONAL;
-	node_event(node, EVENT_CLASS_NODE_CONFIG, EVENT_TYPE_INFO, EVENT_CODE_NODE_CONFIGURED, EVENT_INFO_DUMMY);
-	nmt_master_command(CAN_NMT_START_REMOTE_NODE, node);
+    can_node[node].nmt_state = CAN_NODE_STATE_UNCERTAIN;
+    if (check_node_valid(node) != VALID) return;
+    hbt = CAN_HBT_PRODUCER_MS;
+    if (write_device_object(node, CAN_INDEX_PROD_HBT, 0, (canbyte*)&hbt, 2) != CAN_TRANSTATE_OK) {
+        node_event(node, EVENT_CLASS_NODE_CONFIG, EVENT_TYPE_ERROR, EVENT_CODE_PROD_HBT, EVENT_INFO_DUMMY);
+        return;
+    }
+    can_node[node].nmt_state = CAN_NODE_STATE_PRE_OPERATIONAL;
+    node_event(node, EVENT_CLASS_NODE_CONFIG, EVENT_TYPE_INFO, EVENT_CODE_NODE_CONFIGURED, EVENT_INFO_DUMMY);
+    nmt_master_command(CAN_NMT_START_REMOTE_NODE, node);
 }
 
 void configure_can_nodes(void)
 {
-	cannode node;
+    cannode node;
 
-	for (node = CAN_NODE_ID_MIN; node <= CAN_NODE_ID_MAX; node++) {
-		if (can_node[node].node_status == ON &&
-			can_node[node].nmt_state == CAN_NODE_STATE_INITIALISING) {
-			can_node_config(node);
-		}
-	}
+    for (node = CAN_NODE_ID_MIN; node <= CAN_NODE_ID_MAX; node++) {
+        if (can_node[node].node_status == ON &&
+            can_node[node].nmt_state == CAN_NODE_STATE_INITIALISING) {
+            can_node_config(node);
+        }
+    }
 }
 
 #endif
